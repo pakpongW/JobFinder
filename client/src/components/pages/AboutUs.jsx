c598569d22acf4219db6c983db0b1eba8a9ebacc
mooorkey <i.morkey@msn.com>
2023-03-18 21:26:12 +0700

    finishing about us page

--- client/src/components/pages/AboutUs.jsx
+++ client/src/components/pages/AboutUs.jsx
@@ -1,8 +1,5 @@
 import AnimatedPage from "../AnimatedPage";
-import pic1 from "../picture/pic1.jpg";
-import pic2 from "../picture/wallpaper1.jpg";
-import pic3 from "../picture/wallpaper2.jpg";
-import pic4 from "../picture/wallpaper3.jpg";
+import groupMembers from "../../groupMembers.jsx";
 import banner from "../picture/about_us_pic.jpg";
 import "../../AboutUs.css";
 import { color } from "framer-motion";
@@ -11,38 +8,9 @@ function Box({ children, ...props }) {
   return <div {...props}>{children}</div>;
 }
 
-const groupMembers = [
-  {
-    name: "Sahassawat Suksusiang",
-    picture: pic1,
-    email: "i.morkey@msn.com",
-    stdID: 6301012620197,
-    year: 1,
-  },
-  {
-    name: "Sahassawat Suksusiang",
-    picture: pic1,
-    email: "i.morkey@msn.com",
-    stdID: 6301012620197,
-    year: 1,
-  },
-  {
-    name: "AB1 CDmorkey morkey",
-    picture: pic2,
-  },
-  {
-    name: "AB2 CD",
-    picture: pic3,
-  },
-  {
-    name: "AB CD3",
-    picture: pic4,
-  },
-];
-
 const AboutUs = () => {
   return (
-    <AnimatedPage>
+    <AnimatedPage className="about-us">
       <img src={banner} alt="about us banner" className="banner" />
       <h1 className="center-about">About Us</h1>
       <div className="group-members">
