ad97b9377c919771edfe83c0f9245b7458fee645
pakpong wanachompoo <ryupakpong@gmail.com>
2023-03-21 22:28:55 +0700

    setup to deploy fronend

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // update the <base> tag to use the correct publicPath
-        base: "/public",
-
-        // update the name of your favicon file
-        assetFileNames: "favicon.ico",
-      },
-    },
-  },
+  devTools: false,
 });
